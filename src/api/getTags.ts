6378d00f796ac733b292b562fb5fd7802f39bda8
sohyun lim <limgm7@gmail.com>
2024-07-29 14:53:51 +0900

    Fix: update api endpoint

--- src/api/getTags.ts
+++ src/api/getTags.ts
@@ -3,7 +3,7 @@ import axiosInstance from './axios';
 
 const getTags = async () => {
   try {
-    const response = await axiosInstance.get<TagsResponse>('/tags/ssss');
+    const response = await axiosInstance.get<TagsResponse>('/tags');
     console.log('data for tag response', response.data);
     return response.data;
   } catch (error) {
