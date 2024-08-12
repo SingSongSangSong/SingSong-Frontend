69c0851d7884baeba25fbea80691e44af98965fa
sohyun lim <limgm7@gmail.com>
2024-08-12 21:16:37 +0900

    Feat: change tag icon button style

--- src/components/button/TagIconButton.tsx
+++ src/components/button/TagIconButton.tsx
@@ -16,11 +16,11 @@ const TagIconButton = ({tag, index, onPress, Icon}: TagIconButtonProps) => {
   const viewWidth = deviceWidth * 0.25; // 전체 너비의 80%로 제한
 
   return (
-    <View style={[tw`justify-center items-center m-1`, {width: viewWidth}]}>
+    <View style={[tw`justify-center items-center my-1`, {width: viewWidth}]}>
       <Icon width={50} height={50} onPress={onPress} />
       <TouchableOpacity onPress={onPress}>
         <Text
-          style={tw`text-white text-[10px] mx-6 text-center`}
+          style={tw`text-white text-[10px] mx-4 text-center`}
           numberOfLines={2} // 최대 두 줄로 표시되도록 설정
           ellipsizeMode="tail" // 텍스트가 넘칠 경우 말줄임표(...)로 표시
         >
--- src/components/list/Taglist.tsx
+++ src/components/list/Taglist.tsx
@@ -10,38 +10,20 @@ type TaglistProps = {
 };
 
 const Taglist = ({tags, handleOnTagButton}: TaglistProps) => {
-  // 한 페이지에 보여줄 태그 개수
-  const itemsPerPage = 4;
-
-  // 태그를 페이지별로 그룹화
-  const groupedTags = [];
-  for (let i = 0; i < tags.length; i += itemsPerPage) {
-    groupedTags.push(tags.slice(i, i + itemsPerPage));
-  }
-
   return (
     <View style={tw`w-full mt-2`}>
       <ScrollView
         horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}>
-        {groupedTags.map((group, pageIndex) => (
-          <View
-            key={pageIndex}
-            style={[
-              tw`flex-row`,
-              {width: Dimensions.get('window').width}, // 페이지 너비 설정
-            ]}>
-            {group.map((tag, index) => (
-              <View key={index} style={tw`flex-1 mb-1`}>
-                <TagIconButton
-                  tag={tag}
-                  index={index}
-                  onPress={() => handleOnTagButton(tag)}
-                  Icon={Tag1Icon}
-                />
-              </View>
-            ))}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={tw`flex-row`}>
+        {tags.map((tag, index) => (
+          <View key={index}>
+            <TagIconButton
+              tag={tag}
+              index={index}
+              onPress={() => handleOnTagButton(tag)}
+              Icon={Tag1Icon}
+            />
           </View>
         ))}
       </ScrollView>
