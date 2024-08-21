e073856afdef8502772bcf3d21c5aa1ee1af5ace
sohyun lim <limgm7@gmail.com>
2024-08-21 22:14:19 +0900

    Feat: add crashlytics settings

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {QueryClientProvider} from '@tanstack/react-query';
 // import MainTabNavigator from './src/navigations/tab/MainTabNavigator';
 import AppStackNavigator from './src/navigations/stack/AppStackNavigator';
@@ -8,11 +8,14 @@ import queryClient from './src/api/queryClient';
 // import {GestureHandlerRootView} from 'react-native-gesture-handler';
 // import playlistNavigations from './src/constants/playlistConstants';
 // import GestureRecognizer from 'react-native-swipe-gestures';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 function App(): React.JSX.Element {
   // const onSwipeRight = {navigation}: SplashScreenProps => {
   //   navigation.navigate(playlistNavigations.PLAYLIST);
   // };
+
+  crashlytics().log('App started');
 
   return (
     <QueryClientProvider client={queryClient}>
