6619d72de3f51aafbef0b3ad4fc032ea02d2da1b
sohyun lim <limgm7@gmail.com>
2024-08-30 14:51:16 +0900

    Feat: modify basic playground design

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 import {designatedColor} from '../../constants';
 import tw from 'twrnc';
+import MicIcon from '../../assets/svg/logMic.svg';
 
 function PlaygroundScreen() {
   return (
-    <SafeAreaView
-      style={tw`flex-1 bg-black justify-start items-start pl-4 pt-8`}>
+    <SafeAreaView style={tw`flex-1 bg-black justify-center items-center`}>
       {/* <Text style={tw`text-white font-bold text-lg`}>playground screen</Text> */}
-      <Text style={tw`text-[${designatedColor.PINK}] text-[16] font-bold`}>
-        Coming
-      </Text>
-      <Text style={tw`text-[${designatedColor.PINK}] text-[16] font-bold`}>
-        Soon,
-      </Text>
-      <Text style={tw`text-[${designatedColor.GRAY3}] text-[8] mt-2`}>
-        We're preparing...
-      </Text>
+      <View style={tw`items-center`}>
+        <Text style={tw`text-[${designatedColor.PINK}] text-[10] font-bold`}>
+          Coming Soon
+        </Text>
+        <Text style={tw`text-[${designatedColor.GRAY3}] text-[6] mt-2`}>
+          We're preparing...
+        </Text>
+        <View style={tw`py-4`}>
+          <MicIcon />
+        </View>
+        <Text style={tw`text-[${designatedColor.GRAY1}] text-3 mt-2`}>
+          9월 중 다른 사용자들과 소통할 수 있는 공간이 마련될 예정입니다.
+        </Text>
+        <Text style={tw`text-[${designatedColor.GRAY1}] text-3 mt-2`}>
+          기대해주세요!
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
