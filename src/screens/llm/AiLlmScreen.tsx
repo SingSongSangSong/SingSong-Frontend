d0cd8e99f95eeafc267618751ec1dab04fccfc88
sohyun lim <limgm7@gmail.com>
2024-09-28 00:14:51 +0900

    Feat: add llm info on llm screen. Refs #28

--- src/screens/llm/AiLlmScreen.tsx
+++ src/screens/llm/AiLlmScreen.tsx
@@ -23,6 +23,7 @@ import useAiLlm from '../../hooks/useAiLlm';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {getRandomKeywords} from '../../utils';
 import {ScrollView} from 'react-native-gesture-handler';
+import InfoIcon from '../../assets/svg/Info.svg';
 
 type AiLlmScreenProps = StackScreenProps<
   HomeStackParamList,
@@ -58,11 +59,45 @@ function AiLlmScreen(props: AiLlmScreenProps) {
         <View style={tw`flex-1 w-full justify-start items-center`}>
           <View style={tw`mt-[30%] items-center`}>
             <CustomText style={tw`text-white text-lg font-bold`}>
-              싱송이와 생송이에게 물어보세요
+              싱송이와 생송이에게
+            </CustomText>
+            <CustomText style={tw`text-white text-lg font-bold`}>
+              부르고 싶은 노래를 물어보세요
             </CustomText>
             <View style={tw`items-center my-4`}>
               <SingsongsangsongIcon />
             </View>
+          </View>
+          <View
+            style={tw`bg-[${designatedColor.HOT_TRENDING_COLOR}] mx-4 p-4 rounded-lg mt-2`}>
+            <View style={tw`flex-row items-center mb-2`}>
+              <InfoIcon />
+              <CustomText style={tw`text-white text-[14px] font-bold ml-1`}>
+                더 정확한 추천을 받으려면 아래 키워드를 사용해보세요
+              </CustomText>
+            </View>
+            <CustomText
+              style={tw`text-[${designatedColor.GRAY_E5}] text-sm mb-1`}>
+              1. 부르고 싶은 노래와 비슷한 노래의 가수와 노래 제목을
+              언급해보세요.
+            </CustomText>
+            <CustomText style={tw`text-[${designatedColor.PINK}] text-sm mb-1`}>
+              예시) "빅뱅의 '뱅뱅뱅' 같은 노래 추천해줘"
+            </CustomText>
+            <CustomText
+              style={tw`text-[${designatedColor.GRAY_E5}] text-sm mb-1`}>
+              2. 분위기나 상황을 묘사해보세요. 이를 통해 맞춤형 추천을 받을 수
+              있어요.
+            </CustomText>
+            <CustomText style={tw`text-[${designatedColor.PINK}] text-sm mb-1`}>
+              예시) "편안한 느낌의 노래 추천해줘"
+            </CustomText>
+            <CustomText style={tw`text-[${designatedColor.GRAY_E5}] text-sm`}>
+              3. 특별히 부르고 싶은 특정 연도가 있다면, 그 연도를 언급해보세요.
+            </CustomText>
+            <CustomText style={tw`text-[${designatedColor.PINK}] text-sm mb-1`}>
+              예시) "2010년대 초반의 노래 추천해줘"
+            </CustomText>
           </View>
         </View>
       </TouchableWithoutFeedback>
@@ -72,7 +107,7 @@ function AiLlmScreen(props: AiLlmScreenProps) {
         <ScrollView
           horizontal={true}
           showsHorizontalScrollIndicator={false}
-          contentContainerStyle={tw`flex-row px-4 pt-2`}
+          contentContainerStyle={tw`flex-row px-4 pt-2 bg-[${designatedColor.BACKGROUND_BLACK}]`}
           keyboardShouldPersistTaps="always" // ScrollView 터치 시 키보드를 유지
         >
           {randomKeywords.map((keyword, index) => (
@@ -104,7 +139,7 @@ function AiLlmScreen(props: AiLlmScreenProps) {
       {aiLlmHandler.isLoading && (
         <View
           // bg-opacity-50
-          style={tw`bg-[${designatedColor.BLACK}] w-full h-full justify-center items-center bg-opacity-50`}>
+          style={tw`bg-[${designatedColor.BLACK}] w-full h-full justify-center items-center`}>
           <View style={tw`flex-1 justify-center items-center`}>
             <Image
               source={require('../../assets/gif/animation.gif')}
